56ec8e500a9764720aea3f059605ae719e0642dc
Tín Lý <trungtin.sotech@gmail.com>
2019-07-24 14:57:18 +0700

    Auth before destroy

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,5 +1,5 @@
 class SharesController < ApplicationController
-  before_action :logged_in_user, only: [:create]
+  before_action :logged_in_user, only: [:create, :destroy]
 
   def create
     @share = current_user.shares.build(share_params)
